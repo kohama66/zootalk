785687657cd1a6eaf926be376f89b3d5c8e349c6
kohama66 <l.my.me666666@gmail.com>
2019-09-26 14:21:48 +0900

    create message view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
   root to: "groups#index"
 
-  resources :groups, only: [:index]
+  resources :groups, only: [:index] do
+    resources :messages, only: [:index]
+  end
 end
