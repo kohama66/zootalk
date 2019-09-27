4b3fe1df53b69876307cc2bf07fb2934fb9c01d5
kohama66 <l.my.me666666@gmail.com>
2019-09-27 21:00:30 +0900

    create devise controller

---
+++ app/assets/javascripts/devise.js
@@ -0,0 +1,24 @@
+$(function(){
+  $('.icon-select').on('click', function(e){
+    e.preventDefault();
+    $(".popup").addClass("show");
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  $('.card-action').on('click', function(){
+    $(".popup").removeClass("show");
+    })
+})
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
   root to: "groups#index"
 
+  resources :users, only: [:new]
   resources :groups, only: [:index, :create] do
     resources :messages, only: [:index, :new, :create]
   end
