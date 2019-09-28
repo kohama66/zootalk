3bb5f1e90676a7d4ea61fe332dbe648bafc71dd0
kohama66 <l.my.me666666@gmail.com>
2019-09-28 16:20:31 +0900

    create popup

--- app/assets/javascripts/devise.js
+++ app/assets/javascripts/devise.js
@@ -1,24 +1,31 @@
 $(function(){
-  $('.icon-select').on('click', function(e){
+  
+  $('#1').on('click', function(e){
     e.preventDefault();
-    $(".popup").addClass("show");
+    $(".popup1").addClass("show");
   })
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  $('#2').on('click', function(e){
+    e.preventDefault();
+    $(".popup2").addClass("show");
+  })    
+  $('#3').on('click', function(e){
+    e.preventDefault();
+    $(".popup3").addClass("show");
+  })
+  $('#4').on('click', function(e){
+    e.preventDefault();
+    $(".popup4").addClass("show");
+  })    
+  $('#5').on('click', function(e){
+    e.preventDefault();
+    $(".popup5").addClass("show");
+  })      
   
   $('.card-action').on('click', function(){
-    $(".popup").removeClass("show");
+    $(".popup1").removeClass("show");
+    $(".popup2").removeClass("show");
+    $(".popup3").removeClass("show");
+    $(".popup4").removeClass("show");
+    $(".popup5").removeClass("show");
     })
 })
