afd2e880eb82f1247d5366a4d83a9eba46f838b9
kohama66 <l.my.me666666@gmail.com>
2019-10-03 19:55:00 +0900

    delete point create action

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,9 +1,3 @@
 class PointsController < ApplicationController
 
-  def create
-    point = Point.create(point: 0)
-    user = User.find(current_user.id)
-    user.update(point_id: point.id)
-  end
-
 end
