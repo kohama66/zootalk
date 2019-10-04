b60c7afa94ca00887975dccb673a3a83df2e809d
kohama66 <l.my.me666666@gmail.com>
2019-10-04 20:43:46 +0900

    get user_groups flag

--- app/assets/javascripts/point.js
+++ app/assets/javascripts/point.js
@@ -2,6 +2,8 @@ $(document).on('turbolinks:load', function(){
 $(function(){
 
   $(".frends__icon").on("click", function(){
+    let group_id = $(".main").data("id");
+    console.log(group_id);
     let user_id = $(this).attr("id");
     let point = 1
     $.ajax({
@@ -9,11 +11,12 @@ $(function(){
       type: "POST",
       dataType: "json",
       data: {
-        id: user_id
+        id: user_id,
+        group_id: group_id
       }
     })
-    .done(function(){
-      console.log("ok");
+    .done(function(data){
+      console.log(data);
     })
     .fail(function(){
       console.log("ng");
--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,10 +1,14 @@
 class PointsController < ApplicationController
 
   def create
-    user = User.find(params[:id])
-    point = user.point
-    new_point = up(point)
-    point.update(point: new_point)
+    user = current_user
+    @flag = user.group_users.where(group_id: params[:group_id])
+    # if flag.flag == false
+    #   user = User.find(params[:id])
+    #   point = user.point
+    #   new_point = up(point)
+    #   point.update(point: new_point)
+    # end
     respond_to do |format|
       format.html
       format.json 
